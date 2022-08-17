847ef214912b3f1f61335fef97ae107d58b704fa
EdisonLeeeee <cnljt@outlook.com>
2022-08-18 01:05:22 +0800

    fix instalation on CPU

--- glcore/__init__.py
+++ glcore/__init__.py
@@ -1,7 +1,16 @@
 import torch
 from glcore.sampler import neighbor_sampler_cpu
-from glcore.ops import topk
-from glcore.ops import dimmedian_idx
+
+
+def assert_error(*args, **kwargs):
+    raise RuntimeError("glcore was compiled without support for CUDA.")
+
+
+try:
+    from glcore.ops import topk
+    from glcore.ops import dimmedian_idx
+except ModuleNotFoundError:
+    topk = dimmedian_idx = assert_error
 
 
 __all__ = ["neighbor_sampler_cpu", "topk", "dimmedian_idx"]
